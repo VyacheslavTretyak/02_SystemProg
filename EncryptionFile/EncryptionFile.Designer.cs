2a2cb08e916af34cab79b31388bdbc8717d1b246
VyacheslavTretyak <tretyak1c@gmail.com>
2018-09-05 17:03:33 +0300

    Create project

--- EncryptionFile/EncryptionFile.Designer.cs
+++ EncryptionFile/EncryptionFile.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EncryptionFile
 {
-	partial class Form1
+	partial class EncryptionFile
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,10 +28,17 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.SuspendLayout();
+			// 
+			// EncryptionFile
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.Name = "EncryptionFile";
+			this.Text = "EncryptionFile";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
--- EncryptionFile/EncryptionFile.cs
+++ EncryptionFile/EncryptionFile.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace EncryptionFile
 {
-	public partial class Form1 : Form
+	public partial class EncryptionFile : Form
 	{
-		public Form1()
+		public EncryptionFile()
 		{
 			InitializeComponent();
 		}
